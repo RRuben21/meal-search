e3a42146f535564e3788f662f1009aed380a96b4
RRuben21 <rubenrosa.76@gmail.com>
2024-04-18 15:30:38 -0500

    Data store and meals by letter complete

---
+++ src/store/mutations.js
@@ -0,0 +1,12 @@
+export function setSearchedMeals(state, meals) {
+    state.SearchedMeals = meals || []
+}
+
+export function setMealsByLetter(state, meals) {
+    state.mealsByLetter = meals || []
+}
+
+export function setMealsByIngredients(state, meals) {
+    state.MealsByIngredient = meals || []
+}
+
